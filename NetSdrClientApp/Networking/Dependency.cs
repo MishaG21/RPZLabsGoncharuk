5302d51d7c000852111f88f5151e648c1ba552f7
MishaG21 <mishagoncharuk2005@gmail.com>
2025-11-27 12:02:35 +0200

    Додавання залежностей та тестів для EchoTcpServer
    У проєкті NetSdrClientApp.csproj додано ProjectReference на проєкт EchoTcpServer.
    У NetSdrClientAppTests.csproj виправлено кодування файлу та додано пакет NetArchTest.Rules для тестування архітектури.
    
    Створено клас TestLab5 для перевірки відсутності залежності від EchoTspServer.

---
+++ NetSdrClientApp/Networking/Dependency.cs
@@ -0,0 +1,12 @@
+﻿using EchoTspServer;
+
+namespace NetSdrClientApp
+{
+    public class Dependency
+    {
+        public void CreateServer()
+        {
+            var server = new EchoServer(5000);
+        }
+    }
+}
---
+++ NetSdrClientAppTests/TestLab5.cs
@@ -0,0 +1,22 @@
+﻿using NetArchTest.Rules;
+
+namespace NetSdrClientAppTests
+{
+    public class TestLab5
+    {
+        [Test]
+        public void TestingDependency()
+        {
+            var assembly = typeof(NetSdrClientApp.NetSdrClient).Assembly;
+
+            var result = Types.InAssembly(assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp")
+                .ShouldNot()
+                .HaveDependencyOn("EchoTspServer")
+                .GetResult();
+
+            Assert.IsTrue(result.IsSuccessful);
+        }
+    }
+}
